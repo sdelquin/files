8ae92403117ea5b775295060191f7ada41f2c14b
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-02-08 21:03:39 +0000

    Fix contents chmod

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -2,5 +2,5 @@ if [[ -f .env ]]; then
   export $(cat .env | xargs)
 fi
 
-rsync -avhz contents/* $SSH_USER@$HOST:$CONTENTS_PATH --delete
+rsync -avhz --exclude '_files.zip' contents/* $SSH_USER@$HOST:$CONTENTS_PATH --delete
 ssh $SSH_USER@$HOST "find ${CONTENTS_PATH}* -type 'd' -exec chmod $CONTENTS_CHMOD {} \;"
