e58057ae4020d4a787da65349855526edc28fe63
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-02-09 10:16:47 +0000

    Fix remote files in env

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -2,5 +2,5 @@ if [[ -f .env ]]; then
   export $(cat .env | xargs)
 fi
 
-rsync -avhz --exclude '_files.zip' contents/* $SSH_USER@$HOST:$CONTENTS_PATH --delete
-ssh $SSH_USER@$HOST "find ${CONTENTS_PATH}* -type 'd' -exec chmod $CONTENTS_CHMOD {} \;"
+rsync -avhz --exclude '_files.zip' contents/* $SSH_USER@$HOST:$REMOTE_CONTENTS_PATH --delete
+ssh $SSH_USER@$HOST "find ${REMOTE_CONTENTS_PATH}* -type 'd' -exec chmod $REMOTE_CONTENTS_CHMOD {} \;"
